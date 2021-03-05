fc93f0559b7d7fb6ed350406a1f3f8e43a0310ad
Zoltán Kovács <zoltan.kovacs@codehive.hu>
2021-03-05 12:40:49 +0100

    TypeSupport test.

---
+++ lib/modules/didi-commons/test/utils/TypeSupportTest.ts
@@ -0,0 +1,36 @@
+import { assertEquals, assertStrictEquals } from "../../../../../deps.ts";
+import { TypeSupport } from "../../lib/utils/TypeSupport.ts";
+
+class A {}
+    class B extends A {}
+        class C extends B {}
+    class E extends A {}
+class F {}
+
+Deno.test("SubTypeOf should follow the inheritance chain", () => {
+
+    const isSubType = (subClass: Function, supClass: Function): void => {
+        assertStrictEquals(
+            TypeSupport.subTypeOf(subClass, supClass), true, `${subClass.name} should be subtype of ${supClass.name}`
+        );
+        if (supClass !== subClass) {
+            notSubType(supClass, subClass);
+        }
+    }
+
+    const notSubType = (subClass: Function, supClass: Function): void => {
+        assertStrictEquals(
+            TypeSupport.subTypeOf(subClass, supClass), false, `${subClass.name} shouldn't be subtype of ${supClass.name}`
+        );
+    }
+
+    isSubType(A, A);
+    isSubType(B, A);
+    isSubType(E, A);
+    isSubType(C, B);
+    isSubType(C, A);
+    notSubType(A, F);
+    notSubType(B, E);
+    notSubType(C, E);
+
+})
